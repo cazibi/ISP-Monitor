3a86e1738215e18c2b64992e76847088ef1599a4
Craig Hamilton <craig@cazibi.com>
2018-02-28 19:58:24 +1100

    ready to OS

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
+<!--**************************************************************************************************
+**  
+**  Copyright 2016 Craig Hamilton
+**  
+**  Licensed under the Apache License, Version 2.0 (the "License");
+**  you may not use this file except in compliance with the License.
+**  You may obtain a copy of the License at
+**  
+**      http://www.apache.org/licenses/LICENSE-2.0
+**  
+**  Unless required by applicable law or agreed to in writing, software
+**  distributed under the License is distributed on an "AS IS" BASIS,
+**  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**  See the License for the specific language governing permissions and
+**  limitations under the License.
+**  
+**************************************************************************************************-->
 <html>
 <head>
-  <title>Tesltra Cable Performance</title>
+  <title>ISP Performance</title>
 </head>
 <body>
-  <h1>Telstra Cable Performance</h2>
+  <h1>ISP Performance</h2>
   <table cellspacing="3" cellpadding="3" border="1">
 <?php
   $resultFile = fopen("./isp/results.csv","r");
--- isp/check.sh
+++ isp/check.sh
@@ -1,11 +1,21 @@
-##################################################################################
-##
-##
-##
-##
 #!/bin.sh
-##
-##################################################################################
+###################################################################################################
+##  
+##  Copyright 2016 Craig Hamilton
+##  
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##  
+##      http://www.apache.org/licenses/LICENSE-2.0
+##  
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+##  
+##################################################################################################
 _wd=$(dirname "$0")
 _today=$(date +"%Y-%m-%d")
 _now=$(date +"%H:%M")
